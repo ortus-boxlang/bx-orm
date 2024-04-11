1305572011b90a5ec652989cd1c6186d3132da01
Michael Born <michael@ortussolutions.com>
2024-04-11 15:18:35 -0400

    👌 IMPROVE: Run spotless formatter

--- src/main/java/com/ortussolutions/bifs/ORMFlush.java
+++ src/main/java/com/ortussolutions/bifs/ORMFlush.java
@@ -8,14 +8,14 @@ import ortus.boxlang.runtime.scopes.ArgumentsScope;
 @BoxBIF
 public class ORMFlush extends BIF {
 
-    /**
-     * ExampleBIF
-     *
-     * @param context   The context in which the BIF is being invoked.
-     * @param arguments Argument scope for the BIF.
-     */
-    public String _invoke(IBoxContext context, ArgumentsScope arguments) {
-        return "Hello from an ORMFlush!";
-    }
+	/**
+	 * ExampleBIF
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 */
+	public String _invoke( IBoxContext context, ArgumentsScope arguments ) {
+		return "Hello from an ORMFlush!";
+	}
 
 }
