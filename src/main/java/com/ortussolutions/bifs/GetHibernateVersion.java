86492beba21d464feb970d48753778c3276e20be
Michael Born <michael@ortussolutions.com>
2024-04-11 15:42:13 -0400

    📦 NEW: Add Hibernate dependencies and getHibernateVersion() BIF

---
+++ src/main/java/com/ortussolutions/bifs/GetHibernateVersion.java
@@ -0,0 +1,21 @@
+package com.ortussolutions.bifs;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+
+@BoxBIF
+public class GetHibernateVersion extends BIF {
+
+	/**
+	 * Retrieve the installed Hibernate version.
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 */
+	public String _invoke( IBoxContext context, ArgumentsScope arguments ) {
+		return org.hibernate.Version.getVersionString();
+	}
+
+}
