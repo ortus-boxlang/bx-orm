e70feab50fd8d12bdd3089ea7060650f05e8fde2
Michael Born <michael@ortussolutions.com>
2024-04-15 07:59:27 -0400

    Lots more session factory configuration work

--- src/main/java/com/ortussolutions/SessionFactoryBuilder.java
+++ src/main/java/com/ortussolutions/SessionFactoryBuilder.java
@@ -9,6 +9,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.ortussolutions.config.ORMConnectionProvider;
 import com.ortussolutions.config.ORMKeys;
@@ -26,22 +28,28 @@ import ortus.boxlang.runtime.util.FileSystemUtil;
 
 public class SessionFactoryBuilder {
 
-	private IStruct	ormConfig;
+	private IStruct ormConfig;
 
 	/**
 	 * The application name for this session factory. Used as an identifier in
 	 * hash maps.
 	 */
-	private Key		appName;
+	private Key appName;
 
-	public SessionFactoryBuilder( Key appName, IStruct ormConfig ) {
-		this.appName	= appName;
-		this.ormConfig	= ormConfig;
+	/**
+	 * The logger for this class. We may log warnings or errors if we encounter
+	 * unsupported ORM configuration.
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(SessionFactoryBuilder.class);
+
+	public SessionFactoryBuilder(Key appName, IStruct ormConfig) {
+		this.appName = appName;
+		this.ormConfig = ormConfig;
 	}
 
 	public SessionFactory build() {
 		Configuration configuration = buildConfiguration();
-		getORMMappingFiles( ormConfig ).forEach( configuration::addFile );
+		getORMMappingFiles(ormConfig).forEach(configuration::addFile);
 
 		return configuration.buildSessionFactory();
 	}
@@ -52,29 +60,35 @@ public class SessionFactoryBuilder {
 	 * configuration, but eventually we will support a default datasource.
 	 */
 	private DataSource getORMDataSource() {
-		Key ormDatasource = ormConfig.getAsKey( Key.datasource );
-		if ( ormDatasource != null ) {
-			return BoxRuntime.getInstance().getDataSourceService().get( ormDatasource );
+		Key ormDatasource = ormConfig.getAsKey(Key.datasource);
+		if (ormDatasource != null) {
+			return BoxRuntime.getInstance().getDataSourceService().get(ormDatasource);
 		}
 		throw new BoxRuntimeException(
-		    "ORM configuration is missing 'datasource' key. Default datasources will be supported in a future iteration." );
+				"ORM configuration is missing 'datasource' key. Default datasources will be supported in a future iteration.");
 		// @TODO: Implement this. the hard part is knowing the context.
 		// logger.warn( "ORM configuration is missing 'datasource' key; falling back to
 		// default datasource" );
 		// return currentContext.getConnectionManager().getDefaultDatasourceOrThrow();
 	}
 
-	private List<File> getORMMappingFiles( IStruct ormConfig ) {
+	private List<File> getORMMappingFiles(IStruct ormConfig) {
 		// @TODO: Should we use the application name, or the ORM configuration hash?
 		String xmlMappingLocation = FileSystemUtil.getTempDirectory() + "/orm_mappings/" + getAppName().getName();
-		if ( Boolean.FALSE.equals( ormConfig.getAsBoolean( ORMKeys.autoGenMap ) ) ) {
+		if (Boolean.FALSE.equals(ormConfig.getAsBoolean(ORMKeys.autoGenMap))) {
 			// Skip mapping generation and load the pre-generated mappings from this
 			// location.
-			xmlMappingLocation = ormConfig.getAsString( ORMKeys.cfclocation );
-			throw new BoxRuntimeException( "ORMKeys.autoGenMap: the `false` setting value is currently unsupported." );
+			xmlMappingLocation = ormConfig.getAsString(ORMKeys.cfclocation);
+			throw new BoxRuntimeException("ORMKeys.autoGenMap: the `false` setting value is currently unsupported.");
 		} else {
 			// @TODO: Here we generate entity mappings and populate the temp directory (aka
 			// xmlMappingLocation) with the generated files.
+			// If `ormConfig.getAsBoolean(ORMKeys.savemapping)` is true, we should save the
+			// generated files to `ormConfig.getAsString(ORMKeys.cfclocation)`. Else, we
+			// should save them to the temp directory.
+			// @TODO: Also remember to use the
+			// `ormConfig.getAsBoolean(ORMKeys.skipCFCWithError)` setting to determine
+			// whether to throw exceptions on compile-time errors.
 		}
 		// Regardless of the autoGenMap configuration value, we now have a directory
 		// populated with JPA orm.xml mapping files.
@@ -99,7 +113,7 @@ public class SessionFactoryBuilder {
 		// Alternative test implementation
 		List<File> files = new java.util.ArrayList<>();
 		// Dummy file for testing
-		files.add( Paths.get( "src/test/resources/bx/models/MyEntity.xml" ).toFile() );
+		files.add(Paths.get("src/test/resources/bx/models/MyEntity.xml").toFile());
 		return files;
 	}
 
@@ -107,25 +121,71 @@ public class SessionFactoryBuilder {
 		Configuration configuration = new Configuration();
 
 		// @TODO: generic config goes here
-		if ( ormConfig.containsKey( ORMKeys.namingStrategy ) ) {
+		if (ormConfig.containsKey(ORMKeys.namingStrategy)) {
 			PhysicalNamingStrategy namingStrategy = getNamingStrategyForName(
-			    ormConfig.getAsString( ORMKeys.namingStrategy ) );
-			if ( namingStrategy != null ) {
-				configuration.setPhysicalNamingStrategy( namingStrategy );
+					ormConfig.getAsString(ORMKeys.namingStrategy));
+			if (namingStrategy != null) {
+				configuration.setPhysicalNamingStrategy(namingStrategy);
+			}
+		}
+
+		if (ormConfig.containsKey(ORMKeys.dialect)) {
+			String dialect = ormConfig.getAsString(ORMKeys.dialect);
+			if (dialect != null && !dialect.isBlank()) {
+				logger.warn(
+						"Setting 'dialect' in Hibernate 6.0+ is unnecessary on all Hibernate-supported databases. Ignoring 'dialect' configuration for now.");
+				// @TODO: Until we implement a method to resolve dialect short names, like
+				// `MYSQL` to full
+				// class names, Hibernate will throw errors on startup as 90% of dialect names
+				// will not be found. Should we drop `dialect` support entirely, or should we
+				// add full support for a mostly unnecessary feature?
+				// See warning at
+				// https://docs.jboss.org/hibernate/orm/6.4/javadocs/org/hibernate/cfg/JdbcSettings.html#DIALECT
+				// configuration.setProperty(AvailableSettings.DIALECT, dialect);
+			}
+		}
+
+		if (ormConfig.containsKey(ORMKeys.schema)) {
+			String schema = ormConfig.getAsString(ORMKeys.schema);
+			if (schema != null && !schema.isBlank()) {
+				configuration.setProperty(AvailableSettings.DEFAULT_SCHEMA, schema);
+			}
+		}
+
+		if (ormConfig.containsKey(ORMKeys.catalog)) {
+			String catalog = ormConfig.getAsString(ORMKeys.catalog);
+			if (catalog != null && !catalog.isBlank()) {
+				configuration.setProperty(AvailableSettings.DEFAULT_CATALOG, catalog);
+			}
+		}
+
+		if (ormConfig.containsKey(ORMKeys.sqlScript)) {
+			String sqlScript = ormConfig.getAsString(ORMKeys.sqlScript);
+			if (sqlScript != null && !sqlScript.isBlank()
+					&& ormConfig.getAsString(ORMKeys.dbcreate).equals("dropcreate")) {
+				if (new File(sqlScript).exists()) {
+					// @TODO: We could possibly upgrade this to use the JPA setting:
+					// `JAKARTA_HBM2DDL_CREATE_SCRIPT_SOURCE`, but we'd have to test to see if that
+					// script executes *after* the schema generation (correct behavior), or *in
+					// place of* the schema generation (incorrect behavior).
+					configuration.setProperty(AvailableSettings.HBM2DDL_IMPORT_FILES, sqlScript);
+				} else {
+					logger.error("ORM Configuration `sqlScript` file not found: {}", sqlScript);
+				}
 			}
 		}
 
 		Properties properties = new Properties();
 		// @TODO: Any configuration which needs a specific java type (such as the
 		// connection provider instance) goes here
-		properties.put( AvailableSettings.CONNECTION_PROVIDER, new ORMConnectionProvider( getORMDataSource() ) );
-		configuration.setProperties( properties );
+		properties.put(AvailableSettings.CONNECTION_PROVIDER, new ORMConnectionProvider(getORMDataSource()));
+		configuration.setProperties(properties);
 		return configuration;
 	}
 
-	private PhysicalNamingStrategy getNamingStrategyForName( String name ) {
+	private PhysicalNamingStrategy getNamingStrategyForName(String name) {
 		// @TODO: Use an enum for the naming strategies
-		return switch ( name.toLowerCase() ) {
+		return switch (name.toLowerCase()) {
 			/**
 			 * Historically, the "smart" naming strategy simply converts camelCase to
 			 * MACRO_CASE.
@@ -133,14 +193,15 @@ public class SessionFactoryBuilder {
 			case "smart" -> new MacroCaseNamingStrategy();
 			/**
 			 * The "default" naming strategy is essentially a no-op, and simply returns the
-			 * identifier value unmodified.
+			 * identifier value unmodified. Since this is the default action in Hibernate 6,
+			 * we can skip returning a naming strategy.
 			 */
 			case "default" -> null;
 			/**
 			 * The "cfc" naming strategy allows apps to define their own naming strategy by
 			 * providing a full CFC path.
 			 */
-			default -> new BoxLangClassNamingStrategy( loadBoxlangClassByPath( name ) );
+			default -> new BoxLangClassNamingStrategy(loadBoxlangClassByPath(name));
 		};
 	}
 
@@ -151,10 +212,10 @@ public class SessionFactoryBuilder {
 	 * @param classPath The path to the Boxlang class, either slash or dot
 	 *                  delimited.
 	 */
-	private Class<IClassRunnable> loadBoxlangClassByPath( String classPath ) {
-		String packageName = Paths.get( classPath ).getParent().toString().replace( "/", "." );
-		return RunnableLoader.getInstance().loadClass( Paths.get( classPath ), packageName,
-		    BoxRuntime.getInstance().getRuntimeContext() );
+	private Class<IClassRunnable> loadBoxlangClassByPath(String classPath) {
+		String packageName = Paths.get(classPath).getParent().toString().replace("/", ".");
+		return RunnableLoader.getInstance().loadClass(Paths.get(classPath), packageName,
+				BoxRuntime.getInstance().getRuntimeContext());
 	}
 
 	/**
--- src/main/java/com/ortussolutions/config/ORMConfig.java
+++ src/main/java/com/ortussolutions/config/ORMConfig.java
@@ -5,95 +5,109 @@ import ortus.boxlang.runtime.types.IStruct;
 public class ORMConfig {
 
 	/**
-	 * Specifies whether ColdFusion should automatically generate entity mappings for the persistent CFCs. If autogenmap=false, the mapping should be
+	 * Specifies whether ColdFusion should automatically generate entity mappings
+	 * for the persistent CFCs. If autogenmap=false, the mapping should be
 	 * provided in the form of <code>hbm.xml</code> files.
 	 */
-	private boolean	autoGenMap;
+	private boolean autoGenMap;
 
 	/**
-	 * Allows the engine to manage the Hibernate session. It is recommended not to let the engine manage it for you.
+	 * Allows the engine to manage the Hibernate session. It is recommended not to
+	 * let the engine manage it for you.
 	 *
-	 * Use transaction blocks in order to demarcate your regions that should start, flush and end a transaction.
+	 * Use transaction blocks in order to demarcate your regions that should start,
+	 * flush and end a transaction.
 	 */
-	private boolean	autoManageSession;
+	private boolean autoManageSession;
 
 	/**
-	 * Specifies the location of the configuration file that the secondary cache provider should use. This setting is used only when
+	 * Specifies the location of the configuration file that the secondary cache
+	 * provider should use. This setting is used only when
 	 * secondaryCacheEnabled=true
 	 */
-	private boolean	cacheConfig;
+	private boolean cacheConfig;
 
 	/**
-	 * Specifies the cache provider that ORM should use as a secondary cache. The values can be:
+	 * Specifies the cache provider that ORM should use as a secondary cache. The
+	 * values can be:
 	 *
 	 * Ehcache
 	 * ConcurrentHashMap
 	 *
 	 * The fully qualified name of the class for any other cache provider
 	 */
-	private String	cacheProvider;
+	private String cacheProvider;
 
 	/**
-	 * Specifies the directory (or array of directories) that should be used to search for persistent CFCs to generate the mapping.
+	 * Specifies the directory (or array of directories) that should be used to
+	 * search for persistent CFCs to generate the mapping.
 	 * <p>
 	 * Always specify it or pay a performance startup price.
 	 * <p>
-	 * <strong>Important:</strong> If it is not set, the extension looks at the application directory, its sub-directories, and its mapped directories to
+	 * <strong>Important:</strong> If it is not set, the extension looks at the
+	 * application directory, its sub-directories, and its mapped directories to
 	 * search for persistent CFCs.
 	 */
-	private String	cfclocation;
+	private String cfclocation;
 
 	/**
-	 * This setting defines the data source to be utilized by the ORM. If not used, defaults to the this.datasource in the Application.cfc.
+	 * This setting defines the data source to be utilized by the ORM. If not used,
+	 * defaults to the this.datasource in the Application.cfc.
 	 */
-	private String	datasource;
+	private String datasource;
 
 	/**
 	 * <ul>
-	 * <li><code>update</code> : Creates the database according to your ORM model. It only does incremental updates. It will never remove tables, indexes,
+	 * <li><code>update</code> : Creates the database according to your ORM model.
+	 * It only does incremental updates. It will never remove tables, indexes,
 	 * etc.</li>
-	 * <li><code>dropcreate</code> : Same as above but it destroys the database if it has ny content and recreates it every time the ORM is reloaded.</li>
+	 * <li><code>dropcreate</code> : Same as above but it destroys the database if
+	 * it has ny content and recreates it every time the ORM is reloaded.</li>
 	 * <li><code>none</code> : Does not change the database schema at all.</li>
 	 * </ul>
 	 */
-	private String	dbcreate;
+	private String dbcreate;
 
 	/**
-	 * The dialect to use for your database. By default Hibernate will introspect the datasource and try to figure it out. See the dialects section below.
+	 * The dialect to use for your database. By default Hibernate will introspect
+	 * the datasource and try to figure it out. See the dialects section below.
 	 *
 	 * You can also use the fully Java qualified name of the class.
 	 */
-	private String	dialect;
+	private String dialect;
 
 	/**
-	 * If true, then it enables the ORM event callbacks in entities and globally via the `eventHandler` property.
+	 * If true, then it enables the ORM event callbacks in entities and globally via
+	 * the `eventHandler` property.
 	 */
-	private boolean	eventHandling;
+	private boolean eventHandling;
 
 	/**
 	 * The CFC path of the CFC that will manage the global ORM events.
 	 */
-	private String	eventHandler;
+	private String eventHandler;
 
 	/**
-	 * Specifies if an orm flush should be called automatically at the end of a request. In our opinion this SHOULD never be true. Database persistence
+	 * Specifies if an orm flush should be called automatically at the end of a
+	 * request. In our opinion this SHOULD never be true. Database persistence
 	 * should be done via transaction tags and good transaction demarcation.
 	 */
-	private boolean	flushAtRequestEnd;
+	private boolean flushAtRequestEnd;
 
 	/**
 	 * Specifies if the SQL queries should be logged to the console.
 	 */
-	private boolean	logSQL;
+	private boolean logSQL;
 
 	/**
 	 * Defines the naming convention to use on table and column names.
 	 *
 	 * - default : Uses the table or column names as is
-	 * - smart : This strategy changes the logical table or column name to uppercase.
+	 * - smart : This strategy changes the logical table or column name to
+	 * uppercase.
 	 * - CFC PATH : Use your own CFC to determine naming
 	 */
-	private String	namingStrategy;
+	private String namingStrategy;
 
 	/**
 	 * The path to a custom Hibernate configuration file:
@@ -101,63 +115,70 @@ public class ORMConfig {
 	 * - hibernate.properties
 	 * - hibernate.cfc.xml
 	 */
-	private String	ormConfig;
+	private String ormConfig;
 
 	/**
-	 * If enabled, the ORM will create the Hibernate mapping XML (*.hbmxml) files alongside the entities. This is great for debugging your entities and
+	 * If enabled, the ORM will create the Hibernate mapping XML (*.hbmxml) files
+	 * alongside the entities. This is great for debugging your entities and
 	 * relationships.
 	 *
 	 */
-	private boolean	saveMapping;
+	private boolean saveMapping;
 
 	/**
-	 * The default database schema to use for database connections. This can be overriden at the datasource level.
+	 * The default database schema to use for database connections. This can be
+	 * overriden at the datasource level.
 	 */
-	private String	schema;
+	private String schema;
 
 	/**
-	 * Specifies the default Database Catalog that ORM should use. This can be overriden at the datasource level.
+	 * Specifies the default Database Catalog that ORM should use. This can be
+	 * overriden at the datasource level.
 	 */
-	private String	catalog;
+	private String catalog;
 
 	/**
 	 * Enable the secondary cache or not.
 	 */
-	private boolean	secondaryCacheEnabled;
+	private boolean secondaryCacheEnabled;
 
 	/**
-	 * If true, then the ORM will ignore CFCs that have compile time errors in them. Use false to throw exceptions.
+	 * If true, then the ORM will ignore CFCs that have compile time errors in them.
+	 * Use false to throw exceptions.
 	 */
-	private boolean	skipCFCWithError;
+	private boolean skipCFCWithError;
 
 	/**
-	 * Path to a SQL script file that will be executed after the ORM is initialized. A great way to seed a database.
+	 * Path to a SQL script file that will be executed after the ORM is initialized.
+	 * Only used if dbcreate is set to <code>dropcreate</code>.
 	 */
-	private String	sqlScript;
+	private String sqlScript;
 
 	/**
-	 * Specifies whether the database has to be inspected to identify the missing information required to generate the Hibernate mapping.
+	 * Specifies whether the database has to be inspected to identify the missing
+	 * information required to generate the Hibernate mapping.
 	 *
-	 * The database is inspected to get the column data type, primary key and foreign key information.
+	 * The database is inspected to get the column data type, primary key and
+	 * foreign key information.
 	 */
-	private boolean	useDBForMapping;
+	private boolean useDBForMapping;
 
 	/**
 	 * Constructor
 	 */
-	public ORMConfig( IStruct properties ) {
-		process( properties );
+	public ORMConfig(IStruct properties) {
+		process(properties);
 	}
 
 	/**
 	 * Process the properties
 	 */
-	private void process( IStruct properties ) {
-		if ( properties == null ) {
+	private void process(IStruct properties) {
+		if (properties == null) {
 			return;
 		}
-		if ( properties.containsKey( ORMKeys.autoGenMap ) ) {
-			autoGenMap = properties.getAsBoolean( ORMKeys.autoGenMap );
+		if (properties.containsKey(ORMKeys.autoGenMap)) {
+			autoGenMap = properties.getAsBoolean(ORMKeys.autoGenMap);
 		}
 	}
 }
