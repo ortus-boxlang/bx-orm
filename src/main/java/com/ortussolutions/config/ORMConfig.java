25c6432703c81c0632eaa8f074128442fa85ff21
Michael Born <michael@ortussolutions.com>
2024-04-15 12:15:52 -0400

    More configuration and spotless work

--- src/main/java/com/ortussolutions/config/ORMConfig.java
+++ src/main/java/com/ortussolutions/config/ORMConfig.java
@@ -25,19 +25,14 @@ public class ORMConfig {
 	 * The logger for this class. We may log warnings or errors if we encounter
 	 * unsupported ORM configuration.
 	 */
-	private static final Logger logger = LoggerFactory.getLogger(ORMConfig.class);
-
-	/**
-	 * Underlying storage for all ORM configuration properties.
-	 */
-	private IStruct properties;
+	private static final Logger	logger	= LoggerFactory.getLogger( ORMConfig.class );
 
 	/**
 	 * Specifies whether ColdFusion should automatically generate entity mappings
 	 * for the persistent CFCs. If autogenmap=false, the mapping should be
 	 * provided in the form of <code>hbm.xml</code> files.
 	 */
-	private boolean autoGenMap;
+	private boolean				autoGenMap;
 
 	/**
 	 * Allows the engine to manage the Hibernate session. It is recommended not to
@@ -46,14 +41,14 @@ public class ORMConfig {
 	 * Use transaction blocks in order to demarcate your regions that should start,
 	 * flush and end a transaction.
 	 */
-	private boolean autoManageSession;
+	private boolean				autoManageSession;
 
 	/**
 	 * Specifies the location of the configuration file that the secondary cache
 	 * provider should use. This setting is used only when
 	 * secondaryCacheEnabled=true
 	 */
-	private boolean cacheConfig;
+	private boolean				cacheConfig;
 
 	/**
 	 * Specifies the cache provider that ORM should use as a secondary cache. The
@@ -64,7 +59,7 @@ public class ORMConfig {
 	 *
 	 * The fully qualified name of the class for any other cache provider
 	 */
-	private String cacheProvider;
+	private String				cacheProvider;
 
 	/**
 	 * Specifies the directory (or array of directories) that should be used to
@@ -76,13 +71,13 @@ public class ORMConfig {
 	 * application directory, its sub-directories, and its mapped directories to
 	 * search for persistent CFCs.
 	 */
-	private String cfclocation;
+	private String				cfclocation;
 
 	/**
 	 * Define the data source to be utilized by the ORM. If not used,
 	 * defaults to the this.datasource in the Application.cfc.
 	 */
-	private String datasource;
+	private String				datasource;
 
 	/**
 	 * <ul>
@@ -102,7 +97,7 @@ public class ORMConfig {
 	 * BoxLang.</strong></li>
 	 * </ul>
 	 */
-	private String dbcreate;
+	private String				dbcreate;
 
 	/**
 	 * The dialect to use for your database. By default Hibernate will introspect
@@ -110,30 +105,30 @@ public class ORMConfig {
 	 *
 	 * You can also use the fully Java qualified name of the class.
 	 */
-	private String dialect;
+	private String				dialect;
 
 	/**
 	 * If true, then it enables the ORM event callbacks in entities and globally via
 	 * the `eventHandler` property.
 	 */
-	private boolean eventHandling;
+	private boolean				eventHandling;
 
 	/**
 	 * The CFC path of the CFC that will manage the global ORM events.
 	 */
-	private String eventHandler;
+	private String				eventHandler;
 
 	/**
 	 * Specifies if an orm flush should be called automatically at the end of a
 	 * request. In our opinion this SHOULD never be true. Database persistence
 	 * should be done via transaction tags and good transaction demarcation.
 	 */
-	private boolean flushAtRequestEnd;
+	private boolean				flushAtRequestEnd;
 
 	/**
 	 * Specifies if the SQL queries should be logged to the console.
 	 */
-	private boolean logSQL;
+	private boolean				logSQL;
 
 	/**
 	 * Defines the naming convention to use on table and column names.
@@ -143,7 +138,7 @@ public class ORMConfig {
 	 * uppercase.
 	 * - CFC PATH : Use your own CFC to determine naming
 	 */
-	private String namingStrategy;
+	private String				namingStrategy;
 
 	/**
 	 * The path to a custom Hibernate configuration file:
@@ -151,7 +146,7 @@ public class ORMConfig {
 	 * - hibernate.properties
 	 * - hibernate.cfc.xml
 	 */
-	private String ormConfig;
+	private String				ormConfig;
 
 	/**
 	 * If enabled, the ORM will create the Hibernate mapping XML (*.hbmxml) files
@@ -159,36 +154,37 @@ public class ORMConfig {
 	 * relationships.
 	 *
 	 */
-	private boolean saveMapping;
+	private boolean				saveMapping;
 
 	/**
 	 * The default database schema to use for database connections. This can be
 	 * overriden at the datasource level.
 	 */
-	private String schema;
+	private String				schema;
 
 	/**
 	 * Specifies the default Database Catalog that ORM should use. This can be
 	 * overriden at the datasource level.
 	 */
-	private String catalog;
+	private String				catalog;
 
 	/**
-	 * Enable the secondary cache or not.
+	 * Enable or disable the secondary cache.
 	 */
-	private boolean secondaryCacheEnabled;
+	private boolean				secondaryCacheEnabled;
 
 	/**
-	 * If true, then the ORM will ignore CFCs that have compile time errors in them.
+	 * If true, then the ORM startup will ignore CFCs that have compile time errors
+	 * in them.
 	 * Use false to throw exceptions.
 	 */
-	private boolean skipCFCWithError;
+	private boolean				skipCFCWithError;
 
 	/**
 	 * Path to a SQL script file that will be executed after the ORM is initialized.
 	 * Only used if dbcreate is set to <code>dropcreate</code>.
 	 */
-	private String sqlScript;
+	private String				sqlScript;
 
 	/**
 	 * Specifies whether the database has to be inspected to identify the missing
@@ -197,112 +193,111 @@ public class ORMConfig {
 	 * The database is inspected to get the column data type, primary key and
 	 * foreign key information.
 	 */
-	private boolean useDBForMapping;
+	private boolean				useDBForMapping;
 
 	/**
 	 * Constructor
 	 */
-	public ORMConfig(IStruct properties) {
-		this.properties = properties;
-		process(properties);
+	public ORMConfig( IStruct properties ) {
+		process( properties );
 	}
 
 	/**
 	 * Process the ORM configuration properties and set the private config fields
 	 * accordingly.
 	 */
-	private void process(IStruct properties) {
-		if (properties == null) {
+	private void process( IStruct properties ) {
+		if ( properties == null ) {
 			return;
 		}
-		if (!properties.containsKey(ORMKeys.datasource) || properties.get(ORMKeys.datasource) == null) {
+		if ( !properties.containsKey( ORMKeys.datasource ) || properties.get( ORMKeys.datasource ) == null ) {
 			throw new BoxRuntimeException(
-					"ORM configuration is missing 'datasource' key. Default datasources will be supported in a future iteration.");
+			    "ORM configuration is missing 'datasource' key. Default datasources will be supported in a future iteration." );
 		}
 
 		/**
 		 * Boolean properties: Check key existence, check for null, then cast to
 		 * boolean.
 		 */
-		if (properties.containsKey(ORMKeys.autoGenMap) && properties.get(ORMKeys.autoGenMap) != null) {
-			autoGenMap = properties.getAsBoolean(ORMKeys.autoGenMap);
+		if ( properties.containsKey( ORMKeys.autoGenMap ) && properties.get( ORMKeys.autoGenMap ) != null ) {
+			autoGenMap = properties.getAsBoolean( ORMKeys.autoGenMap );
 		}
 
-		if (properties.containsKey(ORMKeys.autoManageSession) && properties.get(ORMKeys.autoManageSession) != null) {
-			autoManageSession = properties.getAsBoolean(ORMKeys.autoManageSession);
+		if ( properties.containsKey( ORMKeys.autoManageSession ) && properties.get( ORMKeys.autoManageSession ) != null ) {
+			autoManageSession = properties.getAsBoolean( ORMKeys.autoManageSession );
 		}
 
-		if (properties.containsKey(ORMKeys.cacheConfig) && properties.get(ORMKeys.cacheConfig) != null) {
-			cacheConfig = properties.getAsBoolean(ORMKeys.cacheConfig);
+		if ( properties.containsKey( ORMKeys.cacheConfig ) && properties.get( ORMKeys.cacheConfig ) != null ) {
+			cacheConfig = properties.getAsBoolean( ORMKeys.cacheConfig );
 		}
 
-		if (properties.containsKey(ORMKeys.eventHandling) && properties.get(ORMKeys.eventHandling) != null) {
-			eventHandling = properties.getAsBoolean(ORMKeys.eventHandling);
+		if ( properties.containsKey( ORMKeys.eventHandling ) && properties.get( ORMKeys.eventHandling ) != null ) {
+			eventHandling = properties.getAsBoolean( ORMKeys.eventHandling );
 		}
 
-		if (properties.containsKey(ORMKeys.flushAtRequestEnd) && properties.get(ORMKeys.flushAtRequestEnd) != null) {
-			flushAtRequestEnd = properties.getAsBoolean(ORMKeys.flushAtRequestEnd);
+		if ( properties.containsKey( ORMKeys.flushAtRequestEnd ) && properties.get( ORMKeys.flushAtRequestEnd ) != null ) {
+			flushAtRequestEnd = properties.getAsBoolean( ORMKeys.flushAtRequestEnd );
 		}
 
-		if (properties.containsKey(ORMKeys.logSQL) && properties.get(ORMKeys.logSQL) != null) {
-			logSQL = properties.getAsBoolean(ORMKeys.logSQL);
+		if ( properties.containsKey( ORMKeys.logSQL ) && properties.get( ORMKeys.logSQL ) != null ) {
+			logSQL = properties.getAsBoolean( ORMKeys.logSQL );
 		}
 
 		// String properties: Check key existence, check for null, and check for empty
 		// or blank (whitespace-only) strings
-		if (properties.containsKey(ORMKeys.cacheProvider) && properties.get(ORMKeys.cacheProvider) != null
-				&& !properties.getAsString(ORMKeys.cacheProvider).isBlank()) {
-			cacheProvider = properties.getAsString(ORMKeys.cacheProvider);
+		if ( properties.containsKey( ORMKeys.cacheProvider ) && properties.get( ORMKeys.cacheProvider ) != null
+		    && !properties.getAsString( ORMKeys.cacheProvider ).isBlank() ) {
+			cacheProvider = properties.getAsString( ORMKeys.cacheProvider );
 		}
 
-		if (properties.containsKey(ORMKeys.cfclocation) && properties.get(ORMKeys.cfclocation) != null
-				&& !properties.getAsString(ORMKeys.cfclocation).isBlank()) {
-			cfclocation = properties.getAsString(ORMKeys.cfclocation);
+		if ( properties.containsKey( ORMKeys.cfclocation ) && properties.get( ORMKeys.cfclocation ) != null
+		    && !properties.getAsString( ORMKeys.cfclocation ).isBlank() ) {
+			cfclocation = properties.getAsString( ORMKeys.cfclocation );
 		}
 
-		if (properties.containsKey(ORMKeys.datasource) && properties.get(ORMKeys.datasource) != null
-				&& !properties.getAsString(ORMKeys.datasource).isBlank()) {
-			datasource = properties.getAsString(ORMKeys.datasource);
+		if ( properties.containsKey( ORMKeys.datasource ) && properties.get( ORMKeys.datasource ) != null
+		    && !properties.getAsString( ORMKeys.datasource ).isBlank() ) {
+			datasource = properties.getAsString( ORMKeys.datasource );
 		}
 
-		if (properties.containsKey(ORMKeys.dbcreate) && properties.get(ORMKeys.dbcreate) != null
-				&& !properties.getAsString(ORMKeys.dbcreate).isBlank()) {
-			dbcreate = properties.getAsString(ORMKeys.dbcreate);
+		if ( properties.containsKey( ORMKeys.dbcreate ) && properties.get( ORMKeys.dbcreate ) != null
+		    && !properties.getAsString( ORMKeys.dbcreate ).isBlank() ) {
+			dbcreate = properties.getAsString( ORMKeys.dbcreate );
 		}
 
-		if (properties.containsKey(ORMKeys.dialect) && properties.get(ORMKeys.dialect) != null
-				&& !properties.getAsString(ORMKeys.dialect).isBlank()) {
+		if ( properties.containsKey( ORMKeys.dialect ) && properties.get( ORMKeys.dialect ) != null
+		    && !properties.getAsString( ORMKeys.dialect ).isBlank() ) {
 			logger.warn(
-					"Setting 'dialect' in Hibernate 6.0+ is unnecessary on all Hibernate-supported databases. Ignoring 'dialect' configuration for now.");
-			dialect = properties.getAsString(ORMKeys.dialect);
+			    "Setting 'dialect' in Hibernate 6.0+ is unnecessary on all Hibernate-supported databases. Ignoring 'dialect' configuration for now." );
+			dialect = properties.getAsString( ORMKeys.dialect );
 		}
 
-		if (properties.containsKey(ORMKeys.eventHandler) && properties.get(ORMKeys.eventHandler) != null
-				&& !properties.getAsString(ORMKeys.eventHandler).isBlank()) {
-			eventHandler = properties.getAsString(ORMKeys.eventHandler);
+		if ( properties.containsKey( ORMKeys.eventHandler ) && properties.get( ORMKeys.eventHandler ) != null
+		    && !properties.getAsString( ORMKeys.eventHandler ).isBlank() ) {
+			eventHandler = properties.getAsString( ORMKeys.eventHandler );
 		}
-		if (properties.containsKey(ORMKeys.namingStrategy) && properties.get(ORMKeys.namingStrategy) != null
-				&& !properties.getAsString(ORMKeys.namingStrategy).isBlank()) {
-			namingStrategy = properties.getAsString(ORMKeys.namingStrategy);
-		}
-
-		if (properties.containsKey(ORMKeys.ormConfig) && properties.get(ORMKeys.ormConfig) != null
-				&& !properties.getAsString(ORMKeys.ormConfig).isBlank()) {
-			ormConfig = properties.getAsString(ORMKeys.ormConfig);
+		if ( properties.containsKey( ORMKeys.namingStrategy ) && properties.get( ORMKeys.namingStrategy ) != null
+		    && !properties.getAsString( ORMKeys.namingStrategy ).isBlank() ) {
+			namingStrategy = properties.getAsString( ORMKeys.namingStrategy );
 		}
 
-		if (properties.containsKey(ORMKeys.saveMapping) && properties.get(ORMKeys.saveMapping) != null) {
-			saveMapping = properties.getAsBoolean(ORMKeys.saveMapping);
+		if ( properties.containsKey( ORMKeys.ormConfig ) && properties.get( ORMKeys.ormConfig ) != null
+		    && !properties.getAsString( ORMKeys.ormConfig ).isBlank() ) {
+			ormConfig = properties.getAsString( ORMKeys.ormConfig );
 		}
 
-		if (properties.containsKey(ORMKeys.schema) && properties.get(ORMKeys.schema) != null
-				&& !properties.getAsString(ORMKeys.schema).isBlank()) {
-			schema = properties.getAsString(ORMKeys.schema);
+		if ( properties.containsKey( ORMKeys.saveMapping ) && properties.get( ORMKeys.saveMapping ) != null ) {
+			saveMapping = properties.getAsBoolean( ORMKeys.saveMapping );
 		}
 
-		if (properties.containsKey(ORMKeys.catalog) && properties.get(ORMKeys.catalog) != null
-				&& !properties.getAsString(ORMKeys.catalog).isBlank()) {
-			catalog = properties.getAsString(ORMKeys.catalog);
+		if ( properties.containsKey( ORMKeys.schema ) && properties.get( ORMKeys.schema ) != null
+		    && !properties.getAsString( ORMKeys.schema ).isBlank() ) {
+			schema = properties.getAsString( ORMKeys.schema );
+		}
+
+		if ( properties.containsKey( ORMKeys.catalog ) && properties.get( ORMKeys.catalog ) != null
+		    && !properties.getAsString( ORMKeys.catalog ).isBlank() ) {
+			catalog = properties.getAsString( ORMKeys.catalog );
 		}
 	}
 
@@ -325,21 +320,21 @@ public class ORMConfig {
 	public Configuration toHibernateConfig() {
 		Configuration configuration = new Configuration();
 
-		if (this.dbcreate != null) {
+		if ( this.dbcreate != null ) {
 			// @TODO: Use an enum for the dbcreate values, because `create` should be
 			// coerced to `create-only`, and a few others need manipulation as well.
-			configuration.setProperty(AvailableSettings.HBM2DDL_AUTO, this.dbcreate.toString());
+			configuration.setProperty( AvailableSettings.HBM2DDL_AUTO, toHibernateDBCreate().toString() );
 		}
 
-		if (this.namingStrategy != null) {
-			PhysicalNamingStrategy namingStrategy = getNamingStrategyForName(
-					this.namingStrategy);
-			if (namingStrategy != null) {
-				configuration.setPhysicalNamingStrategy(namingStrategy);
+		if ( this.namingStrategy != null ) {
+			PhysicalNamingStrategy loadedNamingStrategy = getNamingStrategyForName(
+			    this.namingStrategy );
+			if ( namingStrategy != null ) {
+				configuration.setPhysicalNamingStrategy( loadedNamingStrategy );
 			}
 		}
 
-		if (this.dialect != null) {
+		if ( this.dialect != null ) {
 			// @TODO: Until we implement a method to resolve dialect short names, like
 			// `MYSQL` to full
 			// class names, Hibernate will throw errors on startup as 90% of dialect names
@@ -350,37 +345,53 @@ public class ORMConfig {
 			// configuration.setProperty(AvailableSettings.DIALECT, dialect);
 		}
 
-		if (this.schema != null) {
-			configuration.setProperty(AvailableSettings.DEFAULT_SCHEMA, schema);
+		if ( this.schema != null ) {
+			configuration.setProperty( AvailableSettings.DEFAULT_SCHEMA, schema );
 		}
 
-		if (this.catalog != null) {
-			configuration.setProperty(AvailableSettings.DEFAULT_CATALOG, catalog);
+		if ( this.catalog != null ) {
+			configuration.setProperty( AvailableSettings.DEFAULT_CATALOG, catalog );
 		}
 
-		if (this.sqlScript != null) {
-			if (Action.CREATE.toString().equals(this.dbcreate)) {
-				if (new File(sqlScript).exists()) {
+		if ( this.sqlScript != null ) {
+			if ( Action.CREATE.toString().equals( this.dbcreate ) ) {
+				if ( new File( sqlScript ).exists() ) {
 					// @TODO: We could possibly upgrade this to use the JPA setting:
 					// `JAKARTA_HBM2DDL_CREATE_SCRIPT_SOURCE`, but we'd have to test to see if that
 					// script executes *after* the schema generation (correct behavior), or *in
 					// place of* the schema generation (incorrect behavior).
-					configuration.setProperty(AvailableSettings.HBM2DDL_IMPORT_FILES, sqlScript);
+					configuration.setProperty( AvailableSettings.HBM2DDL_IMPORT_FILES, sqlScript );
 				} else {
-					logger.error("ORM Configuration `sqlScript` file not found: {}", sqlScript);
+					logger.error( "ORM Configuration `sqlScript` file not found: {}", sqlScript );
 				}
 			} else {
 				logger.warn(
-						"ORM Configuration `sqlScript` is only valid with `dbcreate=dropcreate`. Ignoring for now.");
+				    "ORM Configuration `sqlScript` is only valid with `dbcreate=dropcreate`. Ignoring for now." );
 			}
 		}
+
+		// @TODO: Implement the remaining configuration settings:
+		// - cacheConfig
+		// - cacheProvider
+		// - eventHandling
+		// - eventHandler
+		// - flushAtRequestEnd
+		// - logSQL
+		// - secondaryCacheEnabled
+		// - ormConfig
+
+		// These properties are only used in the SessionFactoryBuilder, and do not need
+		// copying into the Hibernate configuration:
+		// - skipCFCWithError
+		// - useDBForMapping
+		// - saveMapping
 
 		return configuration;
 	}
 
-	private PhysicalNamingStrategy getNamingStrategyForName(String name) {
+	private PhysicalNamingStrategy getNamingStrategyForName( String name ) {
 		// @TODO: Use an enum for the naming strategies
-		return switch (name.toLowerCase()) {
+		return switch ( name.toLowerCase() ) {
 			/**
 			 * Historically, the "smart" naming strategy simply converts camelCase to
 			 * MACRO_CASE.
@@ -396,7 +407,7 @@ public class ORMConfig {
 			 * The "cfc" naming strategy allows apps to define their own naming strategy by
 			 * providing a full CFC path.
 			 */
-			default -> new BoxLangClassNamingStrategy(loadBoxlangClassByPath(name));
+			default -> new BoxLangClassNamingStrategy( loadBoxlangClassByPath( name ) );
 		};
 	}
 
@@ -407,14 +418,14 @@ public class ORMConfig {
 	 * @param classPath The path to the Boxlang class, either slash or dot
 	 *                  delimited.
 	 */
-	private Class<IClassRunnable> loadBoxlangClassByPath(String classPath) {
-		String packageName = Paths.get(classPath).getParent().toString().replace("/", ".");
-		return RunnableLoader.getInstance().loadClass(Paths.get(classPath), packageName,
-				BoxRuntime.getInstance().getRuntimeContext());
+	private Class<IClassRunnable> loadBoxlangClassByPath( String classPath ) {
+		String packageName = Paths.get( classPath ).getParent().toString().replace( "/", "." );
+		return RunnableLoader.getInstance().loadClass( Paths.get( classPath ), packageName,
+		    BoxRuntime.getInstance().getRuntimeContext() );
 	}
 
 	public Action toHibernateDBCreate() {
-		return DBCreate.valueOf(dbcreate.toUpperCase()).toHibernateSetting();
+		return DBCreate.valueOf( dbcreate.toUpperCase() ).toHibernateSetting();
 	}
 
 	/**
@@ -425,7 +436,7 @@ public class ORMConfig {
 	 */
 	private enum DBCreate {
 
-		// Standard settings in CFML:
+	    // Standard settings in CFML:
 		/**
 		 * Update existing schema on Hibernate startup.
 		 */
@@ -485,16 +496,16 @@ public class ORMConfig {
 		TRUNCATE;
 
 		public Action toHibernateSetting() {
-			switch (this) {
-				case CREATE:
+			switch ( this ) {
+				case CREATE :
 					return Action.CREATE_ONLY;
-				case UPDATE:
+				case UPDATE :
 					return Action.UPDATE;
-				case DROPCREATE:
+				case DROPCREATE :
 					return Action.CREATE;
-				case NONE:
+				case NONE :
 					return Action.NONE;
-				default:
+				default :
 					return Action.NONE;
 			}
 		}
