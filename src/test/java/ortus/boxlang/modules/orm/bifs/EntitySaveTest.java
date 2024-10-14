a60a4b1e0315985689967f3bd5993122f0e01d87
Michael Born <michael@ortussolutions.com>
2024-10-14 07:30:33 -0400

    Get EntitySaveTest working solo

--- src/test/java/ortus/boxlang/modules/orm/bifs/EntitySaveTest.java
+++ src/test/java/ortus/boxlang/modules/orm/bifs/EntitySaveTest.java
@@ -2,32 +2,41 @@ package ortus.boxlang.modules.orm.bifs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.hibernate.Transaction;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import ortus.boxlang.modules.orm.ORMService;
 import tools.BaseORMTest;
 
-@Disabled( "Implemented, but not working until we flesh out transaction management." )
+// @TODO: This is implemented, and working, but is a MAJOR hack
+// and needs revisiting to address connection management and transaction management concerns.
+@Disabled( "connection management issues when run in a full suite. Passes when run solo." )
 public class EntitySaveTest extends BaseORMTest {
 
 	@DisplayName( "It can save new entities to the database" )
 	@Test
 	public void testEntitySave() {
+		// @TODO: Plan and implement ORM transaction management.
+		Transaction tx = ORMService.getInstance().getSessionForContext( context ).beginTransaction();
 		// @formatter:off
 		instance.executeSource(
 			"""
-				dev1 = entityNew( "manufacturer", { name : "Dan", role : "Software Engineer" } );
-				dev2 = entityNew( "manufacturer", { name : "Daniel", role : "Software Engineer" } );
+				dev1 = entityNew( "manufacturer", { name : "Audi Corp", address : "101 Audi Way" } );
+				dev2 = entityNew( "manufacturer", { name : "Toyota", address : "101 Toyota Way" } );
 				entitySave( dev1 );
 				ormFlush();
-				result = queryExecute( "SELECT * FROM manufacturers" );
+				// ugh. This is a hack to close the transaction so we don't block the SELECT query.
+				ormGetSession().getTransaction().commit();
+				result = queryExecute( "SELECT * FROM manufacturers WHERE name = 'Audi Corp'" );
 			""",
 			context
 		);
 		// @formatter:on
-		assertEquals( 4, variables.getAsQuery( result ).size() );
-		assertEquals( "Dan", variables.getAsQuery( result ).getRowAsStruct( 0 ).get( "name" ) );
+		// tx.commit();
+		assertEquals( 1, variables.getAsQuery( result ).size() );
+		assertEquals( "Audi Corp", variables.getAsQuery( result ).getRowAsStruct( 0 ).get( "name" ) );
 	}
 
 }
