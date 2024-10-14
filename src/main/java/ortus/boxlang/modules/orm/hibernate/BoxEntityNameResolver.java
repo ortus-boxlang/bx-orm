63f13fb25e608d8c2c7ec2e25b661bea431489e9
Michael Born <michael@ortussolutions.com>
2024-10-14 08:51:06 -0400

    Implement Hibernate's EntityNameResolver for entity name lookups on instantiated classes

--- src/main/java/ortus/boxlang/modules/orm/hibernate/BoxEntityNameResolver.java
+++ src/main/java/ortus/boxlang/modules/orm/hibernate/BoxEntityNameResolver.java
@@ -2,12 +2,29 @@ package ortus.boxlang.modules.orm.hibernate;
 
 import org.hibernate.EntityNameResolver;
 
+import ortus.boxlang.modules.orm.config.ORMKeys;
+import ortus.boxlang.runtime.dynamic.casters.StringCaster;
+import ortus.boxlang.runtime.runnables.IClassRunnable;
+import ortus.boxlang.runtime.types.IStruct;
+
 public class BoxEntityNameResolver implements EntityNameResolver {
 
 	@Override
 	public String resolveEntityName( Object entity ) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException( "Unimplemented method 'resolveEntityName'" );
+		if ( entity instanceof IClassRunnable boxClass ) {
+			IStruct	annotations	= boxClass.getAnnotations();
+			String	result		= null;
+			if ( annotations.containsKey( ORMKeys.entity ) ) {
+				result = StringCaster.cast( annotations.get( ORMKeys.entity ) );
+			} else if ( annotations.containsKey( ORMKeys.entity ) ) {
+				result = StringCaster.cast( annotations.get( ORMKeys.entityName ) );
+			}
+			if ( result == null || result.isBlank() ) {
+				result = boxClass.getClass().getSimpleName().replace( "$bx", "" ).replace( "$cfc", "" );
+			}
+			return result.trim();
+		}
+		return null;
 	}
 
 }
