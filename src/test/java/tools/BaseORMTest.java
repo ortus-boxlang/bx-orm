544330fcf59da780ba96d7eb05ff870cdb20e9e9
Michael Born <michael@ortussolutions.com>
2024-12-05 13:34:11 -0500

    More integration test work

--- src/test/java/tools/BaseORMTest.java
+++ src/test/java/tools/BaseORMTest.java
@@ -7,10 +7,10 @@ import java.nio.file.Paths;
 import java.sql.SQLException;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
+import ortus.boxlang.modules.orm.config.ORMKeys;
 import ortus.boxlang.runtime.BoxRuntime;
 import ortus.boxlang.runtime.context.ApplicationBoxContext;
 import ortus.boxlang.runtime.context.IBoxContext;
@@ -28,48 +28,47 @@ public class BaseORMTest {
 	protected static Key			moduleName	= new Key( "bxorm" );
 	public static Key				result		= Key.of( "result" );
 	public static RequestBoxContext	startupContext;
-	public RequestBoxContext		context;
+	public static RequestBoxContext	context;
 	public IScope					variables;
 
 	@BeforeAll
 	public static void setUp() {
 		System.out.println( "Running @BeforeAll" );
-		instance		= BoxRuntime.getInstance( true, Path.of( "src/test/resources/boxlang.json" ).toString() );
+		instance	= BoxRuntime.getInstance( true );
 
 		// Load the module
-		startupContext	= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
-		BaseORMTest.loadModule( startupContext );
+		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
+		loadModule( context );
+		context.loadApplicationDescriptor( Path.of( "src/test/resources/app/index.bxs" ).toAbsolutePath().toUri() );
 	}
 
 	@AfterAll
 	public static void teardown() throws SQLException {
+		instance.getApplicationService().shutdownApplication( Key.of( "BXORMTest" ) );
+		instance.removeGlobalService( ORMKeys.ORMService );
+		instance.getModuleService().unload( moduleName );
 	}
 
 	@BeforeEach
 	public void setupEach() {
 		System.out.println( "Running @BeforeEach" );
-		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext(), Path.of( "src/test/resources/app/index.bxs" ).toAbsolutePath().toUri() );
-		variables	= context.getScopeNearby( VariablesScope.name );
+		// context = new ScriptingRequestBoxContext( instance.getRuntimeContext(), Path.of( "src/test/resources/app/index.bxs" ).toAbsolutePath().toUri() );
+		variables = context.getScopeNearby( VariablesScope.name );
 		assertNotNull( context.getParentOfType( ApplicationBoxContext.class ) );
-	}
-
-	@AfterEach
-	public void teardownEach() {
-		System.out.println( "Running @AfterEach" );
-		context		= null;
-		variables	= null;
 	}
 
 	protected static void loadModule( IBoxContext context ) {
 		System.out.println( "Loading module" );
-		String physicalPath = Paths.get( "./build/module" ).toAbsolutePath().toString();
-		moduleRecord = new ModuleRecord( physicalPath );
+		if ( !instance.getModuleService().hasModule( moduleName ) ) {
+			String physicalPath = Paths.get( "./build/module" ).toAbsolutePath().toString();
+			moduleRecord = new ModuleRecord( physicalPath );
 
-		instance.getModuleService().getRegistry().put( moduleName, moduleRecord );
+			instance.getModuleService().getRegistry().put( moduleName, moduleRecord );
 
-		moduleRecord
-		    .loadDescriptor( context )
-		    .register( context )
-		    .activate( context );
+			moduleRecord
+			    .loadDescriptor( context )
+			    .register( context )
+			    .activate( context );
+		}
 	}
 }
