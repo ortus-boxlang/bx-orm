d31fb7c65c11bfc331b99c7353cffd313efbfb9d
Michael Born <michael@ortussolutions.com>
2025-01-04 07:54:49 -0500

    More logging in test startup

--- src/test/java/tools/BaseORMTest.java
+++ src/test/java/tools/BaseORMTest.java
@@ -31,10 +31,12 @@ import ortus.boxlang.runtime.context.IBoxContext;
 import ortus.boxlang.runtime.context.IJDBCCapableContext;
 import ortus.boxlang.runtime.context.RequestBoxContext;
 import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
+import ortus.boxlang.runtime.logging.BoxLangLogger;
 import ortus.boxlang.runtime.modules.ModuleRecord;
 import ortus.boxlang.runtime.scopes.IScope;
 import ortus.boxlang.runtime.scopes.Key;
 import ortus.boxlang.runtime.scopes.VariablesScope;
+import ortus.boxlang.runtime.types.IStruct;
 
 public class BaseORMTest {
 
@@ -48,6 +50,7 @@ public class BaseORMTest {
 
 	@BeforeAll
 	public static void setUp() {
+		System.out.println( "Running @BeforeAll" );
 		instance = BoxRuntime.getInstance( true );
 
 		// Load the module
@@ -57,14 +60,36 @@ public class BaseORMTest {
 
 	@AfterAll
 	public static void teardown() {
+		getLogger().debug( "Running @AfterAll" );
 		context.getApplicationListener().onRequestEnd( context, null );
+
+		// @TODO: This should be done in boxlang core if getConnectionManager().shutdown() is called
+		// getLogger().debug( "Shutting down default datasource" );
+		// context.getConnectionManager().getDefaultDatasource().shutdown();
+		// context.getConnectionManager().getCachedDatasources().forEach( ( key, ds ) -> {
+		// try {
+		// getLogger().debug( "Shutting down {} datasource", ds.getUniqueName() );
+		// ds.shutdown();
+		// } catch ( Exception e ) {
+		// getLogger().error( "Error shutting down datasource: " + key, e );
+		// }
+		// } );
+		// getLogger().debug( "Shutting down connection manager" );
+		// context.getConnectionManager().shutdown();
 		instance.getApplicationService().shutdownApplication( Key.of( "BXORMTest" ) );
 	}
 
 	@BeforeEach
 	public void setupEach() {
+		getLogger().debug( "Running @BeforeEach" );
 		variables = context.getScopeNearby( VariablesScope.name );
 
+		String[] registeredDSs = context.getRuntime().getDataSourceService().getNames();
+		for ( String ds : registeredDSs ) {
+			IStruct	stats	= context.getRuntime().getDataSourceService().get( Key.of( ds ) ).getPoolStats();
+			Integer	active	= stats.getAsInteger( Key.of( "activeConnections" ) );
+			getLogger().debug( "Stats for {}: ", ds, stats.asString() );
+		}
 		JDBCTestUtils.resetTables( ( ( IJDBCCapableContext ) context ).getConnectionManager().getDefaultDatasourceOrThrow(), context );
 		JDBCTestUtils.resetAlternateTables( ( ( IJDBCCapableContext ) context ).getConnectionManager().getDatasourceOrThrow( Key.of( "dsn2" ) ), context );
 	}
@@ -72,12 +97,12 @@ public class BaseORMTest {
 	protected static void loadModule( IBoxContext context ) {
 		// Is Derby module loaded?
 		if ( !instance.getModuleService().hasModule( derbyModule ) ) {
-			System.out.println( "Loading Derby module..." );
+			getLogger().debug( "Loading Derby module..." );
 			String derbyPath = moduleDependenciesPath + "/bx-derby";
 
 			if ( !Files.exists( Paths.get( derbyPath ) ) ) {
-				System.out.println( "Derby module not found at " + derbyPath );
-				System.out.println( "Please run 'gradle installModuleDependencies' to install the required modules." );
+				getLogger().debug( "Derby module not found at " + derbyPath );
+				getLogger().debug( "Please run 'gradle installModuleDependencies' to install the required modules." );
 				System.exit( 1 );
 			}
 
@@ -88,12 +113,12 @@ public class BaseORMTest {
 			    .register( context )
 			    .activate( context );
 		} else {
-			System.out.println( "Derby module already loaded, skipping..." );
+			getLogger().debug( "Derby module already loaded, skipping..." );
 		}
 
 		// Is ORM module loaded?
 		if ( !instance.getModuleService().hasModule( ORMKeys.moduleName ) ) {
-			System.out.println( "Loading ORM module..." );
+			getLogger().debug( "Loading ORM module..." );
 			String physicalPath = Paths.get( "./build/module" ).toAbsolutePath().toString();
 			moduleRecord = new ModuleRecord( physicalPath );
 
@@ -104,7 +129,11 @@ public class BaseORMTest {
 			    .register( context )
 			    .activate( context );
 		} else {
-			System.out.println( "ORM module already loaded, skipping..." );
+			getLogger().debug( "ORM module already loaded, skipping..." );
 		}
+	}
+
+	protected static BoxLangLogger getLogger() {
+		return instance.getLoggingService().getLogger( "orm" );
 	}
 }
