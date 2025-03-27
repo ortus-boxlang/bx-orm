98f67344e0df0d808f6bb749b4ae20b2cc8c9734
Jon Clausen <jclausen@ortussolutions.com>
2025-03-27 13:16:56 -0400

    defensive coding

--- src/main/java/ortus/boxlang/modules/orm/bifs/EntityLoad.java
+++ src/main/java/ortus/boxlang/modules/orm/bifs/EntityLoad.java
@@ -79,6 +79,8 @@ public class EntityLoad extends BaseORMBIF {
 			// If the uniqueOrOrder is a struct, we need to move it to options
 			arguments.put( ORMKeys.options, arguments.get( ORMKeys.uniqueOrOrder ) );
 			arguments.remove( ORMKeys.uniqueOrOrder );
+		} else {
+			arguments.put( ORMKeys.options, StringCaster.cast( arguments.get( ORMKeys.options ) ) );
 		}
 		if ( arguments.containsKey( ORMKeys.idOrFilter ) ) {
 			boolean idIsSimpleValue = StringCaster.attempt( arguments.get( ORMKeys.idOrFilter ) ).wasSuccessful();
