e4b7e25c409e56310b7e5076949e27c963034b60
Jon Clausen <jclausen@ortussolutions.com>
2025-04-02 13:17:15 -0400

    cleanup on key usage

--- src/main/java/ortus/boxlang/modules/orm/interceptors/ApplicationListener.java
+++ src/main/java/ortus/boxlang/modules/orm/interceptors/ApplicationListener.java
@@ -27,6 +27,7 @@ import ortus.boxlang.runtime.application.BaseApplicationListener;
 import ortus.boxlang.runtime.context.RequestBoxContext;
 import ortus.boxlang.runtime.events.BaseInterceptor;
 import ortus.boxlang.runtime.events.InterceptionPoint;
+import ortus.boxlang.runtime.scopes.Key;
 import ortus.boxlang.runtime.types.IStruct;
 
 /**
@@ -38,7 +39,8 @@ import ortus.boxlang.runtime.types.IStruct;
 public class ApplicationListener extends BaseInterceptor {
 
 	// The properties to configure the interceptor with
-	private ORMService ormService;
+	private static final String	ORM_LOGGER_NAME	= "orm";
+	private ORMService			ormService;
 
 	/**
 	 * This method is called by the BoxLang runtime to configure the interceptor
@@ -49,7 +51,7 @@ public class ApplicationListener extends BaseInterceptor {
 	@Override
 	public void configure( IStruct properties ) {
 		this.properties	= properties;
-		this.logger		= getRuntime().getLoggingService().getLogger( "orm" );
+		this.logger		= getRuntime().getLoggingService().getLogger( ORM_LOGGER_NAME );
 		this.ormService	= ( ( ORMService ) getRuntime().getGlobalService( ORMKeys.ORMService ) );
 	}
 
@@ -64,14 +66,14 @@ public class ApplicationListener extends BaseInterceptor {
 		    "beforeApplicationListenerLoad fired; checking for ORM configuration"
 		);
 
-		RequestBoxContext		context				= ( RequestBoxContext ) args.get( "context" );
-		BaseApplicationListener	startingListener	= ( BaseApplicationListener ) args.get( "listener" );
-		URI						startingTemplate	= ( URI ) args.get( "template" );
+		RequestBoxContext		context				= ( RequestBoxContext ) args.get( Key.context );
+		BaseApplicationListener	startingListener	= ( BaseApplicationListener ) args.get( Key.listener );
+		URI						startingTemplate	= ( URI ) args.get( Key.template );
 		ORMConfig				config				= ORMConfig.loadFromContext( context );
 
 		// If the starting template is null, it means, the listener is not linked to a template and no Application.bx, so ignore it.
 		if ( startingTemplate != null && config != null ) {
-			this.logger.info( "ORMEnabled, starting up ORM app for [{}]", startingListener.getAppName() );
+			this.logger.debug( "ORMEnabled, starting up ORM app for [{}]", startingListener.getAppName() );
 			this.ormService.startupApp( context, config, startingListener );
 		}
 	}
@@ -81,8 +83,8 @@ public class ApplicationListener extends BaseInterceptor {
 	 */
 	@InterceptionPoint
 	public void onApplicationEnd( IStruct args ) {
-		this.logger.info( "onApplicationEnd fired; Shutting down ORM application" );
-		Application application = ( Application ) args.get( "application" );
+		this.logger.debug( "onApplicationEnd fired; Shutting down ORM application" );
+		Application application = ( Application ) args.get( Key.application );
 		// If the orm app doesn't exist, this is a no-op
 		this.ormService.shutdownApp( ORMService.buildUniqueAppName( application.getName(), application.getStartingListener().getSettings() ) );
 	}
