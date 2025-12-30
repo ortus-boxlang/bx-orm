b766399eeedc7177514b1667e5e204ff7faa3df6
Jon Clausen <jclausen@ortussolutions.com>
2025-12-30 18:06:13 -0500

    Revert "Add support for table/schema/catalog on subclass entities - BLMODULES-118"
    This reverts commit 4711ae0cbb9e9f5bc81f777400a61887edf3b957.

--- src/main/java/ortus/boxlang/modules/orm/mapping/HibernateXMLWriter.java
+++ src/main/java/ortus/boxlang/modules/orm/mapping/HibernateXMLWriter.java
@@ -794,15 +794,18 @@ public class HibernateXMLWriter {
 			classElement.setAttribute( "where", entity.getWhere() );
 		}
 
-		String tableName = entity.getTableName();
-		if ( tableName != null ) {
-			classElement.setAttribute( "table", escapeReservedWords( translateTableName( tableName ) ) );
-		}
-		if ( entity.getSchema() != null ) {
-			classElement.setAttribute( "schema", entity.getSchema() );
-		}
-		if ( entity.getCatalog() != null ) {
-			classElement.setAttribute( "catalog", entity.getCatalog() );
+		// And, if no discriminator or joinColumn is present:
+		if ( entity.isSimpleEntity() ) {
+			String tableName = entity.getTableName();
+			if ( tableName != null ) {
+				classElement.setAttribute( "table", escapeReservedWords( translateTableName( tableName ) ) );
+			}
+			if ( entity.getSchema() != null ) {
+				classElement.setAttribute( "schema", entity.getSchema() );
+			}
+			if ( entity.getCatalog() != null ) {
+				classElement.setAttribute( "catalog", entity.getCatalog() );
+			}
 		}
 
 		// generate keys, aka <id> elements
