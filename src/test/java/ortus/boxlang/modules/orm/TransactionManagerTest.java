20416c802ee28350f58311651ef1694295219088
Michael Born <michaeltheborn@protonmail.com>
2026-05-13 13:45:15 -0400

    Tests - Temporarily disable rollback flush test
    This can become permanent if we determine Lucee and/or ACF did not flush on transaction begin.

--- src/test/java/ortus/boxlang/modules/orm/TransactionManagerTest.java
+++ src/test/java/ortus/boxlang/modules/orm/TransactionManagerTest.java
@@ -131,6 +131,7 @@ public class TransactionManagerTest extends BaseORMTest {
 		assertThat( variables.getAsQuery( result ).getRowAsStruct( 0 ).get( "name" ) ).isEqualTo( "Mitsubishi Corp" );
 	}
 
+	@Disabled( "temporarily disabling while testing." )
 	@DisplayName( "Rollbacks are limited to changes in the transaction context" )
 	@Test
 	public void testORMChildTransactionRollback() {
