4b81aee191d020f6f367bdecfe457d6b39163bda
Michael Born <michaeltheborn@protonmail.com>
2026-05-19 11:39:41 -0400

    TransactionSetSavepoint - flush only if autoManageSession enabled

--- src/main/java/ortus/boxlang/modules/orm/interceptors/TransactionManager.java
+++ src/main/java/ortus/boxlang/modules/orm/interceptors/TransactionManager.java
@@ -109,7 +109,8 @@ public class TransactionManager extends BaseInterceptor {
 			    "No ORM application found during transaction request.  Either the ORM service is not properly configured or the application has not yet started." );
 			return;
 		}
-		ORMContext ormContext = ORMContext.getForContext( context.getParentOfType( IJDBCCapableContext.class ) );
+		ORMContext	ormContext	= ORMContext.getForContext( context.getParentOfType( IJDBCCapableContext.class ) );
+		ORMConfig	config		= ormContext.getConfig();
 		ormApp.getDatasources().forEach( datasource -> {
 			Session ormSession = ormContext.getSession( datasource );
 
@@ -121,8 +122,9 @@ public class TransactionManager extends BaseInterceptor {
 				    datasource.getName()
 				);
 			}
-
-			ormSession.flush();
+			if ( config.autoManageSession ) {
+				ormSession.flush();
+			}
 		} );
 	}
 
